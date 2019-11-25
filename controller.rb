266c9a8e367f3a5dfd4d3062a24daddd736dfbd7
Paul McManus <paulmcmanus2021@hotmail.com>
2019-11-25 17:22:46 +0000

    MVP with some css

--- controller.rb
+++ controller.rb
@@ -3,3 +3,14 @@ require ('sinatra/contrib/all') if development?
 
 require_relative ('./models/game')
 also_reload ('./game/*')
+
+get '/' do
+  erb(:home)
+end
+
+get '/:move1/:move2' do
+  move1 = params['move1']
+  move2 = params['move2']
+  @result = Game.check(move1, move2)
+  erb(:result)
+end
